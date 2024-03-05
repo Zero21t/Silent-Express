b317b15dce4f8919d851d38c81c7362e9e59fc24
Aman Toor <amanst.04s@gmail.com>
2024-03-04 22:16:58 -0500

    v idk lol

--- recieve.sh
+++ recieve.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 source Express.sh
+
+# Define variables
+HOST="$HOST"
+USERNAME="$USER"
+PASSWORD="$PASSWD"
+REMOTE_DIR="$DIRECTORY"
+FILE_DIRECTORY="$FILE"
+
+# Transfer file to remote server
+sftp $USERNAME@$HOST 
+$PASSWORD
+cd "$REMOTE_DIRECTORY"
+get "$FILE_DIRECTORY"
